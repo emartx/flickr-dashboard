100dcb7079566aa4a7d2bddf7c76b9f91c1774db
emitex1 <emad.armoun@gmail.com>
2025-03-22 06:19:43 +0100

    convert to hook

--- packages/frontend/src/infra/useApi.ts
+++ packages/frontend/src/infra/useApi.ts
@@ -2,46 +2,48 @@ import axios from "axios";
 import { GeneralResult } from "@flickr-dashboard/core/src/types";
 import { useAuth } from "../context/AuthContext";
 
-const callApi = async <T>(url: string): Promise<GeneralResult<T>> => {
-  const { firebaseUser }  = useAuth();
-  const token = await firebaseUser.getIdToken();
+const useApi = () => {
+  const { firebaseUser } = useAuth();
 
-  try {
-    const response = await axios.get<GeneralResult<T>>(
-      url,
-      {
-        headers: {
-          "Content-Type": "Application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-    const json = response.data;
+  const callApi = async <T>(url: string): Promise<GeneralResult<T>> => {
+    const token = await firebaseUser.getIdToken();
+    
+		try {
+			const response = await axios.get<GeneralResult<T>>(url, {
+				headers: {
+					"Content-Type": "Application/json",
+					Authorization: `Bearer ${token}`,
+				},
+			});
+			const json = response.data;
 
-    if ('isDone' in json) {
-      if (!json.isDone) {
-        console.warn(`Server controlled error: ${json.message}`);
-        return json;
-      }
+			if ("isDone" in json) {
+				if (!json.isDone) {
+					console.warn(`Server controlled error: ${json.message}`);
+					return json;
+				}
 
-      return json;
-    } else {
-      return {
-        isDone: false,
-        status: 500,
-        message: "Invalid response structure",
-        data: null,
-      }
-    }
-  } catch (error) {
-    console.error("Unhandled error:", error);
-    return {
-      isDone: false,
-      status: 500,
-      message: (error as Error).message || 'Unknown error',
-      data: null,
-    };
-  }
-}
+				return json;
+			} else {
+				return {
+					isDone: false,
+					status: 500,
+					message: "Invalid response structure",
+					data: null,
+				};
+			}
+		} catch (error) {
+			console.error("Unhandled error:", error);
+			return {
+				isDone: false,
+				status: 500,
+				message: (error as Error).message || "Unknown error",
+				data: null,
+			};
+		}
+	};
 
-export default callApi;
+  return { callApi };
+};
+
+export default useApi;
